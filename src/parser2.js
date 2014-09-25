f7772c2a3385c5fdf828e89cc23dda1334cb766f
Matt Benton <matt@mattbenton.net>
2014-09-24 17:57:10 -0700

    improved parser

--- src/parser2.js
+++ src/parser2.js
@@ -1,193 +1,306 @@
-var operatorRegExp = /^(?:\+|\+\+|-|--|={1,3}|\*|\/|&&|\|\|)/;
-var wordRegExp = /^[a-zA-Z$_]+[a-zA-Z0-9$_.]*/;
-var reservedWordRegExp = /^true|false|null|undefined|new|Date$/;
-var numberRegExp = /^(?:\.\d+|\d+|\d+\.\d+)/;
+acute.parser2 = (function () {
 
-function grab ( buffer, pattern ) {
-  var match = buffer.contents.substr(buffer.index).match(pattern);
-  if ( match ) {
-    buffer.index += match[0].length - 1;
-    return match;
-  }
-}
+  // Captures any sequence of operators.
+  var operatorCharsRegExp = /^[+\-=%*\/&|]+/;
 
-function createBuffer ( contents ) {
-  return {
-    contents: contents,
-    index: 0,
-    length: contents.length
-  };
-}
+  // Supported operators:
+  // +, -, =, ==, ===, *, /, &&, ||
+  var validOperatorRegExp = /^(?:\+{1}|-{1}|={1,3}|\*|\/|&&|\|\|)$/;
 
-function tokenize ( expr ) {
-  var tokens = [];
-  var buffer = createBuffer(expr);
+  // A path such as "user", "user.name" or "user.colors[2]".
+  var pathRegExp = /^[a-zA-Z$_]+[a-zA-Z0-9$_.]*/;
 
-  var inString = false;
-  var quoteChr = null;
+  // Reserved words.
+  var reservedWordRegExp = /^true|false|null|undefined|new|Date$/;
 
-  var wordStartIndex;
-  var match;
+  // Matches anything that lools like an interger or float.
+  var numberRegExp = /^-?(?:\.\d+|\d+|\d+\.\d+)/;
 
-  while ( buffer.index < buffer.length ) {
-    var chr = buffer.contents.charAt(buffer.index);
-    // console.log("chr", chr);
-
-    if ( inString ) {
-      if ( chr === quoteChr ) {
-        inString = false;
-        quoteChr = null;
-        tokens.push({ type: "S", value: buffer.contents.substr(wordStartIndex, buffer.index - wordStartIndex) });
-      }
-    } else if ( chr === "'" || chr === '"' ) {
-      inString = true;
-      quoteChr = chr;
-      wordStartIndex = buffer.index + 1;
-    }
-    else if ( (match = grab(buffer, operatorRegExp)) ) {
-      tokens.push({ type: "O", value: match[0] });
-    }
-    else if ( (match = grab(buffer, wordRegExp)) ) {
-      tokens.push({
-        type: reservedWordRegExp.test(match[0]) ? "L" : "W",
-        value: match[0]
-      });
-    }
-    else if ( (match = grab(buffer, numberRegExp)) ) {
-      tokens.push({ type: "L", value: match[0] });
-    }
-    else if ( chr !== " " ) {
-      tokens.push({ type: "L", value: chr });
-    }
-
-    buffer.index++;
+  function parse ( expr ) {
+    var tokens = tokenize(expr);
+    groupOperators(tokens);
+    console.log(JSON.stringify(tokens, null, 2));
+    var source = getSource(tokens);
+    return source;
   }
 
-  console.log("tokens", JSON.stringify(tokens, null, 2));
-  return tokens;
-}
-
-function groupTokens ( tokens ) {
-  var op = findHighestPriorityOperator(tokens);
-  if ( !op ) {
-    console.log("@#@");
-  }
-  while ( op ) {
-    groupOpToken(op, tokens);
-    op = findHighestPriorityOperator(tokens);
-  }
-  console.log("tokens", JSON.stringify(tokens, null, 2));
-}
-
-function groupOpToken ( op, tokens ) {
-  var index = op.index;
-  if ( index > 0 && index < tokens.length - 1 ) {
-    var group = {
-      type: "G",
-      lhs: tokens[index - 1],
-      op: op.token,
-      rhs: tokens[index + 1]
-    };
-    tokens.splice(index - 1, 3, group);
-  } else {
-    throw new Error("cannot group op token on edge: " + JSON.stringify(op));
-  }
-}
-
-var priorities = {
-  "!": 10,
-  "*": 9, "%": 9, "/": 9,
-  "+": 8, "-": 8,
-  "==": 7, "===": 7, "!=": 7, "!==": 7,
-  "&&": 6,
-  "||": 5,
-  "=": 4
-};
-
-function findHighestPriorityOperator ( tokens ) {
-  var highest = 0;
-  var index = 0;
-
-  for ( var i = 0, len = tokens.length; i < len; i++ ) {
-    var token = tokens[i];
-    if ( token.type === "O" ) {
-      var p = priorities[token.value];
-      if ( p > highest ) {
-        highest = p;
-        index = i;
-      }
+  /**
+  * Tests a pattern against the current index of the buffer and
+  * if it matches, moves the index to the position where the match ended.
+  *
+  * @param {Buffer} buffer
+  * @param {RegExp} pattern
+  * @return {Array|null} the matched regular expression.
+  */
+  function grab ( buffer, pattern ) {
+    var match = buffer.contents.substr(buffer.index).match(pattern);
+    if ( match ) {
+      buffer.index += match[0].length - 1;
+      return match;
     }
   }
 
-  if ( highest ) {
+  /**
+  * Creates a `Buffer`, a simple object that keeps track of the contents,
+  * current index and length of a string.
+  *
+  * @param {String}
+  * @return {Buffer}
+  */
+  function createBuffer ( contents ) {
     return {
-      token: tokens[index],
-      index: index
+      contents: contents,
+      index: 0,
+      length: contents.length
     };
   }
-}
 
-function transform ( tokens ) {
-  var source = "";
+  /**
+  * Parses an expression into an array of tokens in the form:
+  *
+  * `{ type: "type", value: "value" }`.
+  *
+  * Types:
+  *
+  * - "P" - Path of on the scope.
+  * - "O" - Operator.
+  * - "S" - String.
+  * - "L" - Literal.
+  *
+  * TODO: Combine store strings as literal tokens.
+  * TODO: Handle escaped quotes.
+  *
+  * @param {String} expr
+  * @param {Array} an array of tokens.
+  */
+  function tokenize ( expr ) {
+    var tokens = [];
+    var stack = [];
+    var buffer = createBuffer(expr);
 
-  for ( var i = 0, len = tokens.length; i < len; i++ ) {
-    var token = tokens[i];
-    // if ( token.value !== "(" && token.value !== ")" ) {
-      source += transformToken(token);
-    // }
-  }
+    var inString = false;
+    var quoteChr = null;
 
-  return source;
-}
+    var wordStartIndex;
+    var match;
 
-function transformToken ( token ) {
-  var type = token.type;
-  var value = token.value;
-
-  if ( type === "G" ) {
-
-    if ( token.op.value === "=" ) {
-      var source = "(";
-      if ( token.lhs.type === "W" ) {
-        source += "scope.set('" + token.lhs.value + "', ";
-      } else {
-        throw new Error("Cannot assign left value of non variable");
+    while ( buffer.index < buffer.length ) {
+      var chr = buffer.contents.charAt(buffer.index);
+      if ( inString ) {
+        if ( chr === quoteChr ) {
+          inString = false;
+          quoteChr = null;
+          tokens.push({ type: "S", value: buffer.contents.substr(wordStartIndex, buffer.index - wordStartIndex) });
+        }
+      } else if ( chr === "'" || chr === '"' ) {
+        inString = true;
+        quoteChr = chr;
+        wordStartIndex = buffer.index + 1;
       }
-      return source + transformToken(token.rhs) + ")";
+      else if ( (match = grab(buffer, numberRegExp)) ) {
+        tokens.push({ type: "L", value: match[0] });
+      }
+      else if ( (match = grab(buffer, operatorCharsRegExp)) ) {
+        if ( validOperatorRegExp.test(match[0]) ) {
+          tokens.push({ type: "O", value: match[0] });
+        } else {
+          throw new Error("Invalid operator '" + match[0] + "'");
+        }
+      }
+      else if ( (match = grab(buffer, pathRegExp)) ) {
+        tokens.push({
+          type: reservedWordRegExp.test(match[0]) ? "L" : "P",
+          value: match[0]
+        });
+      }
+      else if ( chr === "(" ) {
+        // Push the current level of tokens on the stack
+        // to create a deeper level.
+        stack.push(tokens);
+        tokens = [];
+      }
+      else if ( chr === ")" ) {
+        // Done with this level of tokens.
+        var children = tokens;
+
+        if ( stack.length ) {
+          tokens = stack.pop();
+        } else {
+          throw new Error("Unbalanced parentheses");
+        }
+
+        if ( tokens.length ) {
+          // There is a higher level of tokens on the stack, so add this level as children of it.
+          var parent = tokens[tokens.length - 1];
+          if ( parent.type === "P" ) {
+            parent.children = children;
+          } else {
+            throw new Error("Exec non-word");
+          }
+        } else {
+          // We're back at the root level, so prepend all of the tokens
+          // back, making sure to add back the opening and closing parentheses.
+          children.unshift({ type: "L", value: "(" });
+          children.push({ type: "L", value: ")" });
+          tokens = children.concat(tokens);
+        }
+      }
+      else if ( chr !== " " ) {
+        tokens.push({ type: "L", value: chr });
+      }
+
+      buffer.index++;
     }
 
-    if ( token.lhs.value === ")" ) {
-      return transformToken(token.op) + transformToken(token.rhs) + ")";
+    return tokens;
+  }
+
+  /**
+  * Loops through a list of tokens, grouping all operators
+  * with their left-hand side (lhs) and right-hand side (rhs)
+  * tokens based on the standard order of operations.
+  *
+  * @param {Array<Token>} tokens
+  */
+  function groupOperators ( tokens ) {
+    var op = findHighestPriorityOperator(tokens);
+    while ( op ) {
+      createGroupToken(op, tokens);
+      op = findHighestPriorityOperator(tokens);
+    }
+  }
+
+  /**
+  * Replaces the operator token in the list of tokens
+  * with a `group` token that points to the left and right
+  * tokens on each side of the operator, as well as the operator
+  * itself.
+  *
+  * @param {Token} op An operator token.
+  * @param {Array<Token>} tokens
+  */
+  function createGroupToken ( op, tokens ) {
+    var index = op.index;
+    if ( index > 0 && index < tokens.length - 1 ) {
+      var group = {
+        type: "G",
+        lhs: tokens[index - 1],
+        op: op.token,
+        rhs: tokens[index + 1]
+      };
+      tokens.splice(index - 1, 3, group);
+    } else {
+      throw new Error("cannot group op token on edge: " + JSON.stringify(op));
+    }
+  }
+
+  // A map of operator values to their priority.
+  // Higher values are prioritized first. Based on the standard order of
+  // operations: http://en.wikipedia.org/wiki/Order_of_operations.
+  var operatorPriority = {
+    "!": 10,
+    "*": 9, "%": 9, "/": 9,
+    "+": 8, "-": 8,
+    "==": 7, "===": 7, "!=": 7, "!==": 7,
+    "&&": 6,
+    "||": 5,
+    "=": 4
+  };
+
+  /**
+  * Finds the highest priority operator token in a list of tokens.
+  *
+  * @param {Array} tokens An array of tokens.
+  */
+  function findHighestPriorityOperator ( tokens ) {
+    var highest = 0;
+    var index = 0;
+
+    for ( var i = 0, len = tokens.length; i < len; i++ ) {
+      var token = tokens[i];
+      if ( token.type === "O" ) {
+        var p = operatorPriority[token.value];
+        if ( p > highest ) {
+          highest = p;
+          index = i;
+        }
+      }
     }
 
-    // return source + transformToken(token.op) + transformToken(token.rhs) + ")";
-    return "(" + transformToken(token.lhs) + transformToken(token.op) + transformToken(token.rhs) + ")";
-  }
-  else if ( type === "L" ) {
-    return value;
-  }
-  else if ( type === "O" ) {
-    if ( value !== "(" && value !== ")" ) {
-      return " " + value + " ";
+    if ( highest ) {
+      return {
+        token: tokens[index],
+        index: index
+      };
     }
-    return "";
-    // return "" + value + "";
   }
-  else if ( type === "S" ) {
-    return "'" + value + "'";
-  }
-  else if ( type === "W" ) {
-    return "scope.get('" + value + "')";
-  }
-  return "####";
-}
 
-// var expr = "name = 'Matthew', true";
-// var expr = "10 == 7 + 3";
-// var expr = "{ 's-minimized': widget.isMinimized, 's-loading': widget.isLoading }";
-// var expr = "2 + 5 * 10 * 3";
-// var expr = "(10 - 4) / 2";
-var expr = "matt(), fred()";
-var tokens = tokenize(expr);
-groupTokens(tokens);
-console.debug(transform(tokens));
+  /**
+  * Transforms a list of tokens to source code.
+  *
+  * @param {Array<Token>} tokens
+  * @return {String} Source code representation of tokens.
+  */
+  function getSource ( tokens ) {
+    var source = "";
+    for ( var i = 0, len = tokens.length; i < len; i++ ) {
+      var token = tokens[i];
+      source += transformToken(token);
+    }
+    return source;
+  }
+
+  /**
+  * Transforms a token to source code.
+  *
+  * @param {Token} token
+  * @return {String} Source code representation of token.
+  */
+  function transformToken ( token ) {
+    var type = token.type;
+    var value = token.value;
+
+    if ( type === "G" ) {
+      if ( token.op.value === "=" ) {
+        // Assignment.
+        var source = "(";
+        if ( token.lhs.type === "P" ) {
+          source += "scope.set('" + token.lhs.value + "', ";
+        } else {
+          throw new Error("Cannot assign left value of non variable");
+        }
+        return source + transformToken(token.rhs) + ")";
+      }
+      if ( token.lhs.value === ")" ) {
+        return transformToken(token.op) + transformToken(token.rhs) + ")";
+      }
+      return "(" + transformToken(token.lhs) + transformToken(token.op) + transformToken(token.rhs) + ")";
+    }
+    else if ( type === "L" ) {
+      return value;
+    }
+    else if ( type === "O" ) {
+      if ( value !== "(" && value !== ")" ) {
+        return " " + value + " ";
+      }
+      return "";
+    }
+    else if ( type === "S" ) {
+      return "'" + value + "'";
+    }
+    else if ( type === "P" ) {
+      if ( token.children ) {
+        return "scope.exec('" + value + "',[" + getSource(token.children) + "])";
+      }
+      return "scope.get('" + value + "')";
+    }
+    return "####";
+  }
+
+  return {
+    parse: parse,
+    groupOperators: groupOperators,
+    getSource: getSource
+  };
+
+}());
